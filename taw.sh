1fe90f821cf63e9ef319d033ef9f527142f95e42
marcaur <alienated0110@gmail.com>
2020-11-02 00:40:47 -0500

    Calculated amount of time in days

--- taw.sh
+++ taw.sh
@@ -24,23 +24,41 @@
 
 #read -p "How old are you?" AGE
 
-# AMOUNT OF HOURS A YEAR SPENT WORKING 
+# AMOUNT OF HOURS A YEAR SPENT WORKING
 # account for non-integer input values
 read -p "How many hours do you spend at work each week?" WRKHR
 read -p "How much vacation time do you get a year?(weeks)" VT
-WRKWEEK=52-$((VT))
-HRYEAR=$((WRKHR*WRKWEEK))
+WRKWEEK=52-$((VT)) #AMOUNT OF WEEKS YOU WORK IN A YEAR W/VACAY
+HRYEAR=$((WRKHR*WRKWEEK)) #TOTAL AMOUNT OF HOURS SPENT AT WORK IN A YEAR
+echo -e "You have spent $HRYEAR hours at work over the last $SPAN year"
+# if $SPAN > 1:
+#   year+='s'
 
 
-# DAYS A WEEK SPENT WORKING
+
+
+# AMOUNT OF DAYS A YEAR SPENT WORKING
 # INCLUDE CONSTRAINT OF 7 DAYS
-#read -p "How many days a week do you work?" DYWEEK
-# VACATION TO BE SUBTRACTED FROM TOTAL TIME
+COUNTME=0
+while [ $COUNTME -lt 1 ]; do
+  read -p "How many days a week do you work? " DYWEEK
+  if (($DYWEEK <= 7)); then
+    COUNTME+=1
+  elif (($DYWEEK > 7)); then
+    echo "Please enter a number between 1-7 "
+    continue
+  fi
+done
+echo "You spend $DYWEEK days at work a week"
 
-# LENGTH OF TIME YOU HAVE  BEEN WORKING
+# VACATION TO BE SUBTRACTED FROM TOTAL TIME
+VDAY=$VT*7 # HOW MANY DAYS OFF DO YOU GET A YEAR
+DYYEAR=$((DYWEEK*52-VDAY))
+echo "You spend $DYYEAR days a year at work"
+
+#LENGTH OF TIME YOU HAVE  BEEN WORKING
 #read -p "How long have you been working? (in years)" SPAN
 
 
 
 #echo -e "You have been working for $SPAN years"
-echo -e "You have spent $HRYEAR hours at work in the past year"
